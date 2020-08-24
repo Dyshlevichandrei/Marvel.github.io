9a2e884b4e2a35e8129ee8a1f8e97b5c4b14e935
Andrei <45993907+Dyshlevichandrei@users.noreply.github.com>
2020-08-24 20:50:22 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,16 @@
+/* mobile navigation*/
+$(".burger-menu").on('click', function(e) {
+    $(".burger-menu").toggleClass("burger-menu_active");
+    $(".list").toggleClass("list-active");
+    $(".nav").toggleClass("nav-active");
+    $(".logo-img").toggleClass("logo-img-active");
+});
+
+
+
+let controls = document.querySelector('.controls');
+let controlsSpan = document.querySelectorAll('.controls-span');
+
+
+console.log(controlsSpan)
+//# sourceMappingURL=data:application/json;charset=utf8;base64,eyJ2ZXJzaW9uIjozLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiIiwic291cmNlcyI6WyJqcy9tYWluLmpzIl0sInNvdXJjZXNDb250ZW50IjpbIi8qIG1vYmlsZSBuYXZpZ2F0aW9uKi9cclxuJChcIi5idXJnZXItbWVudVwiKS5vbignY2xpY2snLCBmdW5jdGlvbihlKSB7XHJcbiAgICAkKFwiLmJ1cmdlci1tZW51XCIpLnRvZ2dsZUNsYXNzKFwiYnVyZ2VyLW1lbnVfYWN0aXZlXCIpO1xyXG4gICAgJChcIi5saXN0XCIpLnRvZ2dsZUNsYXNzKFwibGlzdC1hY3RpdmVcIik7XHJcbiAgICAkKFwiLm5hdlwiKS50b2dnbGVDbGFzcyhcIm5hdi1hY3RpdmVcIik7XHJcbiAgICAkKFwiLmxvZ28taW1nXCIpLnRvZ2dsZUNsYXNzKFwibG9nby1pbWctYWN0aXZlXCIpO1xyXG59KTtcclxuXHJcblxyXG5cclxubGV0IGNvbnRyb2xzID0gZG9jdW1lbnQucXVlcnlTZWxlY3RvcignLmNvbnRyb2xzJyk7XHJcbmxldCBjb250cm9sc1NwYW4gPSBkb2N1bWVudC5xdWVyeVNlbGVjdG9yQWxsKCcuY29udHJvbHMtc3BhbicpO1xyXG5cclxuXHJcbmNvbnNvbGUubG9nKGNvbnRyb2xzU3BhbikiXSwiZmlsZSI6ImpzL21haW4uanMifQ==
